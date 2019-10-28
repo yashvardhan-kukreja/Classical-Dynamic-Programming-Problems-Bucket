cc42335bfbd2fcc4d37498ebcbb21afaa7d92f64
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2019-10-28 23:18:07 +0530

    Added: Memoization solution for Longest Increasing Subsequence

---
+++ Python/Longest Increasing Subsequence/memoization.py
@@ -0,0 +1,22 @@
+def lis(arr, arr_len):
+    cache = [1 for i in range(arr_len)]  ## Initially, every element, individually, can be a part of the LIS
+
+    ## For every 0 <= i < arr_len, cache[i] corresponds to the solution of LIS for arr[0 .. i]
+    for i in range(1, arr_len):
+        for j in range(i):
+            if arr[j]<=arr[i]:   ## Current element is greater than iterator element, then, well, it CAN be included in LIS
+                already_existing_solution = cache[i]
+                new_answer = cache[j]+1
+                if new_answer>already_existing_solution: ## But only include it if it produce a higher  number (LIS) than already existing one i.e. cache[j]>cache[i]
+                    cache[i] = new_answer
+
+    return max(cache)
+
+n = int(input("Please enter the number of elements in the sequence: "))
+
+arr = []
+for i in range (n):
+    arr.append(int(input("Please enter the element at index-{}: ".format(i))))
+
+print ("Length of the Longest Increasing Subsequence: {}".format(lis(arr, n)))
+
