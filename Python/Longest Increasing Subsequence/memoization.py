bd78b4ecfc7b6e13b972633fc65eadf2aca00a07
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2019-10-28 23:33:26 +0530

    Added: some more comments

--- Python/Longest Increasing Subsequence/memoization.py
+++ Python/Longest Increasing Subsequence/memoization.py
@@ -5,10 +5,11 @@ def lis(arr, arr_len):
     for i in range(1, arr_len):
         for j in range(i):
             if arr[j]<=arr[i]:   ## Current element is greater than iterator element, then, well, it CAN be included in LIS
-                already_existing_solution = cache[i]
-                new_answer = cache[j]+1
-                if new_answer>already_existing_solution: ## But only include it if it produce a higher  number (LIS) than already existing one i.e. cache[j]>cache[i]
-                    cache[i] = new_answer
+                already_existing_solution = cache[i] ## Already existing solution till i-th element of the original array
+                new_solution = cache[j]+1 ## This is going to be solution if j-th element is added to the LIS (coz well, j-th element is added to the LIS)
+                ## Now, let's just see, if we should add j-th element to the solution or not considering if it produces a better solution 
+                if new_solution>already_existing_solution: ## But only include it if it produce a higher  number (LIS) than already existing one i.e. cache[j]>cache[i]
+                    cache[i] = new_solution
 
     return max(cache)
 
